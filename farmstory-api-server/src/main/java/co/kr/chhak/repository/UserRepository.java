bd7e2568167ef86b2fa413b9673710f9fa46b97d
chhak0503 <chhak0503@gmail.com>
2024-11-20 10:55:17 +0900

    farmstory study

---
+++ farmstory-api-server/src/main/java/co/kr/chhak/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package co.kr.chhak.repository;
+
+import co.kr.chhak.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
